00570cc5c06a573770c69485fe8390ac1591c03e
Tom <thomas.hodson@ecmwf.int>
2024-04-10 11:17:27 +0100

    refactor

--- src/ionbeam/parsers/__init__.py
+++ src/ionbeam/parsers/__init__.py
@@ -1,26 +1,18 @@
-# #
-# # (C) Copyright 2023 ECMWF.
-# #
-# # This software is licensed under the terms of the Apache Licence Version 2.0
-# # which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
-# # In applying this licence, ECMWF does not waive the privileges and immunities
-# # granted to it by virtue of its status as an intergovernmental organisation nor
-# # does it submit to any jurisdiction.
-# #
-from pathlib import Path
-
-
-from ..core.plugins import find_plugin
-from ..core.bases import Parser
+#
+# (C) Copyright 2023 ECMWF.
+#
+# This software is licensed under the terms of the Apache Licence Version 2.0
+# which can be obtained at http://www.apache.org/licenses/LICENSE-2.0.
+# In applying this licence, ECMWF does not waive the privileges and immunities
+# granted to it by virtue of its status as an intergovernmental organisation nor
+# does it submit to any jurisdiction.
+#
 
 from .csv import CSVParser
 from .csv_file_chunker import CSVChunker
+from .generate_metadata import GenerateMetaData
+from .filter import DateFilter
 
 __all__ = [
     "CSVParser",
 ]
-
-
-def load_parser(name: str, **kwargs) -> Parser:
-    klass = find_plugin(Path(__file__).parent, __name__, "parser", name)
-    return klass(**kwargs)
