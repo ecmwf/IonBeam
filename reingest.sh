ac14f800977d9700ad8bd485388fafd15ad704c0
Tom Hodson <thomas.hodson@ecmwf.int>
2025-01-23 14:34:23 +0000

    write reingest.sh

---
+++ reingest.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+cd /home/math/IonBeam
+export ODC_ENABLE_WRITING_LONG_STRING_CODEC=1
+echo >> cron_logs_ingest.txt
+echo Ingestion `date` >> cron_logs_ingest.txt
+/home/math/.venv/bin/python -m ionbeam \
+     ./config -vvv \
+    --env ewc \
+    --sources  acronet meteotracker smart_citizen_kit\
+    --no-download \
+    --ingest-to-pipeline \
+    --overwrite-fdb \
+    --reingest-from=2025-01-21 \
+    --logfile cron_logs_ingest.txt
