ec99b76c1aa1b140420b0740228c08a35718e04d
Joshua Harwood <joshua.harwood@ecmwf.int>
2025-11-21 11:25:40 +0100

    tidy

--- docs/conf.py
+++ docs/conf.py
@@ -2,14 +2,6 @@
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# NOTE: This configuration uses autoapi without automatic generation and
-# instead uses directives (see api.rst). This is done because with just
-# autoapi auto generation all modules must be imported. In ource case this
-# would mean we need to complie our python bindings. Using autoapi with
-# directives allows to use 'static analysis' mode to extract docstrings.default_mod
-# See: https://sphinx-autoapi.readthedocs.io/en/stable/how_to.html#how-to-transition-to-autodoc-style-documentation
-# See: https://sphinx-autoapi.readthedocs.io/en/stable/reference/directives.html
-
 import datetime
 import sys
 from pathlib import Path
